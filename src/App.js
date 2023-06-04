c75c2e280d53b79682d085416edde7ab270a5f68
Claudemir Romano <clauromano.crs@gmail.com>
2023-06-04 20:47:17 -0300

    navbootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import { Div, DivWhite } from "./global-styled-components";
+import { MainSection } from "./components/main_section/main_section";
+import { Div } from "./global-styled-components";
 import { callChatGpt } from "./services/get_api_chat_gpt";
 
 function App() {
 
-  // async function dataTeste() {
-  //   try {
-  //     let teste = await callChatGpt();
-  //     return teste;
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
-
-  // console.log('dataTeste', dataTeste)
-
   return (
     <>
       <Div className="verde">
-        HELLO WORLD!
+        <MainSection/>
       </Div>
       <Div className="branco">
         VAI CURINTIA
