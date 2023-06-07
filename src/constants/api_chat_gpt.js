997985262b88ddf5da61233f96c59437a7d17fb6
ClaudemirRomanoDosSantos-GustavoCastanho-LeilaRibeiro-RenataAntoniassi-ViniciusMello <duartecgustavo@outlook.com>
2023-06-07 19:39:59 -0300

    .

--- src/constants/api_chat_gpt.js
+++ src/constants/api_chat_gpt.js
@@ -1,2 +1,2 @@
 export const API_CHAT_GPT = 'https://api.openai.com/v1/chat/completions'
-export const OPENAI_API_KEY = 'xxxxxxxxxx'
+export const OPENAI_API_KEY = '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'
